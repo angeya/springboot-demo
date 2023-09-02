6006dffae636ea076f18738a932be97e3afbbbb5
angeya <1571858518@qq.com>
2023-09-02 13:20:17 +0800

    Initial commit

---
+++ src/main/java/top/angeya/App.java
@@ -0,0 +1,16 @@
+package top.angeya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: angeya
+ * @Date: 2023/9/2 12:51
+ * @Description: 启动类
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/test/java/top/angeya/TestDemo.java
@@ -0,0 +1,21 @@
+package top.angeya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: angeya
+ * @Date: 2023/9/2 13:12
+ * @Description: 单元测试示例
+ */
+@SpringBootTest
+public class TestDemo {
+
+    // 依赖注入
+
+    @Test
+    public void test() {
+        System.out.println("测试示例");
+    }
+
+}
